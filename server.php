566d4aa65755488afc2a0bb8fc0ee3904ffe5597
DavideGila <davide.gila@gmail.com>
2023-12-01 16:04:47 +0100

    made if isset and json encode

--- server.php
+++ server.php
@@ -5,4 +5,16 @@ $filecontent = file_get_contents("todo-list.json");
 
 $list = json_decode($filecontent, true);
 
+if(isset($_POST["task"])){
+    $new_task = [
+        "text" => $_POST["task"],
+        "done" => false,
+    ];
+    array_push($list, $new_task);
+    file_put_contents("todo-list.json", json_encode($list));
+}
+
+header('Content-Type: application/json');
+
+echo json_encode($list);
 ?>
